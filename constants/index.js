7ac06d53bed72709034f879ba4b5694d79e9da0c
olawale-o <omoogunolawale@gmail.com>
2022-02-11 13:49:43 +0100

    Hide env variables

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,17 @@
+require('dotenv').config()
+const { PORT, MONGODB_USERNAME, MONGODB_PASSWORD, NODE_ENV, APP_NAME} = process.env;
+
 module.exports = {
   mongoOptions: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+  },
+
+  envVariables: {
+    PORT,
+    MONGODB_USERNAME,
+    MONGODB_PASSWORD,
+    NODE_ENV,
+    APP_NAME,
   },
 };
