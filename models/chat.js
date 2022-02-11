2a80ffb4579742a4dbd9ee56612d2cbfbbd67551
olawale-o <omoogunolawale@gmail.com>
2022-02-11 10:33:34 +0100

    Create chat model

---
+++ models/chat.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const chatSchema = new Schema({
+  chat_id: {type: String, required: true, unique: true},
+  sender_id: {type: Schema.Types.ObjectId, ref: 'User'},
+  receiver_id: {type: Schema.Types.ObjectId, ref: 'User'},
+  message: {type: String, required: true},
+  status: {type: String, enum: ['pending', 'delivered', 'read'], default: 'pending'},
+  created_at: {type: Date, default: Date.now},
+});
+
+module.exports = new mongoose.model('Chat', chatSchema);
