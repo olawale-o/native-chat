078be9477ab3d21015bb89ba8d7b54aec36c0e16
olawale-o <omoogunolawale@gmail.com>
2022-02-14 16:59:29 +0100

    Chore

--- config/app.js
+++ config/app.js
@@ -13,4 +13,12 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.use('/v1/api', [require('../routes/auth'), require('../routes/user')]);
 
+app.use((req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
 module.exports = app;
--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-require('dotenv').config()
+
+const { mongoOptions, envVariables } = require('./constants');
+const { PORT, MONGODB_USERNAME, MONGODB_PASSWORD, NODE_ENV, APP_NAME} = envVariables;
+
 const http = require('http');
 
 const app = require('./config/app');
-
 const server = http.createServer(app);
 
 const { Server } = require('socket.io');
 
+const socket = require('./socket')
+const connect = require('./database');
+
 const io = new Server(server);
 
-io.on('connection', (socket) => {
-  console.log('Nova conexão', socket.id);
+socket(io);
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', `You said: ${msg} to server`);
-  });
+const uri = NODE_ENV === 'development' ? 
+'mongodb://localhost:27017/crime' :
+`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.kr6st.mongodb.net/${APP_NAME}?retryWrites=true&w=majority`;
 
-  socket.on('disconnect', () => console.log('User disconnected'));
-});
 
-server.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+connect(uri, mongoOptions).then(
+  () => {
+    server.listen(PORT || 3000, () => {
+      console.log('Server started on port 3000');
+    });
+    console.log('connected to database')    
+  },
+  err => console.log(err)
+);
